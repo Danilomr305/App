ae4a47b5a8fd05e3bb9639ea2c4bd81efbf0e2c7
Danilomr305 <danilomr305@gmail.com>
2023-11-26 17:12:24 -0300

    w

--- brasileirao_app/lib/main.dart
+++ brasileirao_app/lib/main.dart
@@ -1,12 +1,18 @@
+import 'dart:js';
+
+import 'package:brasileirao_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'meu_aplicativo.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        
+        ChangeNotifierProvider(create: (context) => AuthService()),
       ],
+      child: const MeuApp(),
     ),
   );
 }
---
+++ brasileirao_app/lib/meu_aplicativo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
