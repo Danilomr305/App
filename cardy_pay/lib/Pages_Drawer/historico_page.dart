212a890711633ec863af0840f4c09c485ae7dcb3
Danilomr305 <128266870+Danilomr305@users.noreply.github.com>
2024-04-09 14:42:34 -0300

    d

--- cardy_pay/lib/Pages_Drawer/historico_page.dart
+++ cardy_pay/lib/Pages_Drawer/historico_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+import '../configs/app_settings.dart';
+import '../repository/conta_repository.dart';
 
 class HistoricoPage extends StatefulWidget {
   const HistoricoPage({super.key});
@@ -8,8 +13,59 @@ class HistoricoPage extends StatefulWidget {
 }
 
 class _HistoricoPageState extends State<HistoricoPage> {
+
+  double saldo = 0;
+  late NumberFormat real;
+  late ContaRepository conta;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    conta = context.watch<ContaRepository>();
+    final loc = context.read<AppSettings>().locale;
+    real = NumberFormat.currency(locale: loc['locale'], name: loc['name']);
+    saldo = conta.saldo;
+    
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black45,
+        centerTitle: WidgetsApp.debugAllowBannerOverride,
+        title: const Text(
+          'Historico',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1,
+            color: Colors.white
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: loadHistorico(),
+          )
+        ],
+      ),
+    );
+  }
+
+  loadHistorico() {
+
+    final historico = conta.historico;
+    final date = DateFormat('dd/MM/yyyy - hh:mm');
+    List<Widget> widgets = [];
+
+    for (var  operacao in historico) {
+      widgets.add(ListTile(
+        title: Text(operacao.moeda.nome),
+        subtitle: Text(date.format(operacao.dataOperacao)),
+        trailing: 
+        Text(real.format((operacao.moeda.preco * operacao.quantidade))),
+      ));
+      widgets.add( const Divider());
+    }
+    return Column(
+      children: widgets,
+    );
   }
 }
